c6462417fdce2e9644dbed1bf2f42bdd868af1ca
YuGyeom-Tara <kyk5680579@yonsei.ac.kr>
2020-10-17 13:03:02 +0900

    models 변경

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Movie, Theme, Comment
+
 # Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Theme)
+admin.site.register(Comment)
--- main/models.py
+++ main/models.py
@@ -9,11 +9,11 @@ class Theme(models.Model):
 class Movie(models.Model):
     # 의견이 1개라도 작성된 Movie
     title = models.CharField(max_length=100)
-    #theme = models.ManyToManyField(Theme)
-    comment = models.PositiveIntegerField(blank = False)
+    theme = models.ManyToManyField(Theme)
+    #comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE)
     possible = models.BooleanField()
     subrun = models.BooleanField()
     
 class Comment(models.Model):
-    #movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     content = models.TextField()
