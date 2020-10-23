ce8f262418d6d1ced90d2dd8034d44fe3fa0c6a7
Jeong YoonHo <uno8941@gmail.com>
2020-10-23 19:56:23 +0900

    home style 완료

---
+++ main/static/js/home.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.querySelector(".vote-container");
+    $("html, body").animate({ scrollTop: $(".vote-container").position().top }, 700);
+});
